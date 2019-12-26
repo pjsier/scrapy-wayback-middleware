01c45cb0df74e60e749901ffaffe45d5a36f394a
pjsier <pjsier@gmail.com>
2019-12-26 15:57:19 -0500

    feat: only rate limit specific download slot

--- scrapy_wayback_middleware/middleware.py
+++ scrapy_wayback_middleware/middleware.py
@@ -1,7 +1,11 @@
 import json
-import time
+from datetime import datetime, timedelta
 
 import scrapy
+from scrapy import signals
+from scrapy.core.downloader import _get_concurrency_delay
+
+SLOT_KEY = "_wayback_slot"
 
 
 class WaybackMiddleware:
@@ -15,12 +19,48 @@ class WaybackMiddleware:
     def __init__(self, crawler, is_post=False):
         self.crawler = crawler
         self.is_post = is_post
+        self.delay_until = datetime.now()
+        self.crawler.signals.connect(
+            self._response_downloaded, signal=signals.response_downloaded
+        )
 
-    def process_spider_input(self, response, spider):
+    @property
+    def delay_seconds(self):
+        """Calculate delay in seconds if a delay is set, otherwise return 0"""
+        current_time = datetime.now()
+        if current_time < self.delay_until:
+            return (self.delay_until - datetime.now()).seconds
+        return 0
+
+    def _response_downloaded(self, response, request, spider):
+        """
+        Adjust Wayback Machine slot download delay if 429 response codes are returned.
+
+        Based on the AutoThrottle extension.
+        """
+        key = request.meta.get("download_slot")
+        if key != SLOT_KEY:
+            return
+        slot = self.crawler.engine.downloader.slots.get(SLOT_KEY)
+
+        # Get default values for concurrency and delay
+        ip_concurrency = self.crawler.settings.getint("CONCURRENT_REQUESTS_PER_IP")
+        domain_concurrency = self.crawler.settings.getint(
+            "CONCURRENT_REQUESTS_PER_DOMAIN"
+        )
+        _conc, delay = _get_concurrency_delay(
+            ip_concurrency if ip_concurrency else domain_concurrency,
+            spider,
+            self.crawler.settings,
+        )
+
         if response.status == 429:
-            self.crawler.engine.pause()
-            time.sleep(60)
-            self.crawler.engine.unpause()
+            # Update the delay datetime if the response has a 429 status code
+            self.delay_until = datetime.now() + timedelta(seconds=60)
+            slot.delay = self.delay_seconds
+        elif self.delay_seconds < delay and slot.delay > delay:
+            # If the status code is not 429 and the delay is passed, reset slot delay
+            slot.delay = delay
 
     def process_spider_output(self, response, result, spider):
         """Process normally, adding Wayback Machine requests"""
@@ -45,6 +85,7 @@ class WaybackMiddleware:
                         "dont_obey_robotstxt": True,
                         "dont_redirect": True,
                         "dont_retry": True,
+                        "download_slot": SLOT_KEY,
                     },
                     dont_filter=True,
                 )
@@ -57,6 +98,7 @@ class WaybackMiddleware:
                         "dont_obey_robotstxt": True,
                         "dont_redirect": True,
                         "dont_retry": True,
+                        "download_slot": SLOT_KEY,
                     },
                 )
 
--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,9 +1,10 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock
 
 import pytest
 import scrapy
 
 from scrapy_wayback_middleware import WaybackMiddleware
+from scrapy_wayback_middleware.middleware import SLOT_KEY
 
 
 @pytest.fixture(autouse=True)
@@ -27,6 +28,7 @@ def test_process_spider_output(response_mock):
         scrapy.Request.call_args[0][0]
         == "https://web.archive.org/save/https://example.com"
     )
+    assert scrapy.Request.call_args[1]["meta"]["download_slot"] == SLOT_KEY
 
 
 def test_ignore_archive_urls(response_mock):
@@ -46,10 +48,11 @@ def test_get_item_urls(monkeypatch, scrapy_request, response_mock):
 
 
 def test_use_post_request(response_mock):
-    middleware = WaybackMiddleware(MagicMock, is_post=True)
+    middleware = WaybackMiddleware(MagicMock(), is_post=True)
     [res for res in middleware.process_spider_output(response_mock, [], None)]
     scrapy.Request.assert_called_once()
     assert scrapy.Request.call_args[0][0] == "https://pragma.archivelab.org"
+    assert scrapy.Request.call_args[1]["meta"]["download_slot"] == SLOT_KEY
 
 
 def test_ignore_post_requests(scrapy_request, response_mock):
@@ -59,10 +62,36 @@ def test_ignore_post_requests(scrapy_request, response_mock):
     scrapy.Request.assert_not_called()
 
 
-def test_pause_on_429(response_mock):
+def test_set_slot_delay_on_429(response_mock):
     response_mock.status = 429
+    request_mock = MagicMock()
+    request_mock.meta = {"download_slot": SLOT_KEY}
     crawler_mock = MagicMock()
-    crawler_mock.engine = MagicMock()
+    slot_mock = Mock()
+    slot_mock.delay = 0
+    crawler_mock.engine.downloader.slots.get.return_value = slot_mock
     middleware = WaybackMiddleware(crawler_mock)
-    middleware.process_spider_input(response_mock, None)
-    crawler_mock.engine.pause.assert_called_once()
+    middleware._response_downloaded(response_mock, request_mock, None)
+
+    crawler_mock.engine.downloader.slots.get.assert_called_once()
+    assert middleware.delay_seconds > 0
+    assert slot_mock.delay > 0
+
+
+def test_reset_slot_delay(monkeypatch, response_mock):
+    conc_delay_mock = MagicMock()
+    conc_delay_mock.return_value = 0, 1
+    monkeypatch.setattr(
+        "scrapy_wayback_middleware.middleware._get_concurrency_delay", conc_delay_mock
+    )
+    request_mock = MagicMock()
+    request_mock.meta = {"download_slot": SLOT_KEY}
+    crawler_mock = MagicMock()
+    slot_mock = Mock()
+    slot_mock.delay = 10
+    crawler_mock.engine.downloader.slots.get.return_value = slot_mock
+    middleware = WaybackMiddleware(crawler_mock)
+    middleware._response_downloaded(response_mock, request_mock, None)
+
+    crawler_mock.engine.downloader.slots.get.assert_called_once()
+    assert slot_mock.delay == 1
